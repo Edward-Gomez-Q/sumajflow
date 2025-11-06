e2eaed548eef935c2a498d05798e7108f0f21121
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2025-11-06 12:57:58 -0400

    first commit

---
+++ src/main/java/ucb/edu/bo/sumajflow/SumajflowApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.sumajflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumajflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumajflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/edu/bo/sumajflow/SumajflowApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.sumajflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumajflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
